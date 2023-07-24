9172fc5a363dd553168a2454f13ce0b8b4248307
jones <selvajones150@gmail.com>
2023-07-24 10:35:23 +0530

    Function

---
+++ js/main.js
@@ -0,0 +1,23 @@
+   function sum(mark1,mark2,mark3,mark4,mark5)
+   {
+	let Total=mark1+mark2+mark3+mark4+mark5;
+	let average=Total/sum.length;
+	document.write("Total &nbsp"+Total+"&nbsp"+"<br>");
+	return average;
+   }
+   
+   var Jones=sum(60,75,45,83,90)
+   document.write("Jones Average is &nbsp"+Jones+"<br>");
+   
+   var Santhose=sum(90,70,49,81,70)
+   document.write("Santhose Average is &nbsp"+Santhose+"<br>");
+	
+   var Siva=sum(80,95,60,63,50)
+   document.write("Siva Average is &nbsp"+Siva+"<br>");
+   
+   var Prabha=sum(68,75,45,83,100)
+   document.write("pranha Average is &nbsp"+Prabha+"<br>");
+   
+   var Uthaya=sum(93,60,85,33,40)
+   document.write("Uthaya Average is &nbsp"+Uthaya+"<br>");
+   
